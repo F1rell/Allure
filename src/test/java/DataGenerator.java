a1bd1a8521b37f709a0e2b3ec9db320653d6ee45
F1rell <ratmir777mustafin@mail.ru>
2022-04-16 21:37:30 +0700

    Allure not start

---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,36 @@
+import com.github.javafaker.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+
+    public static String city() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.options().option("Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир", "Ульяновск", "Челябинск", "Ярославль");
+    }
+
+    public static String date(int plusDay) {
+        return LocalDate.now().plusDays(plusDay).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String surNameAndFirstName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String phone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static String notCorrectPhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.options().option("123");
+    }
+
+    public static String notWorkingName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.options().option("Свиридова") + " " + faker.options().option("Алёна");
+    }
+}
